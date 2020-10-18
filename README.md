6aa308af9348bc627e81c8419314d03e823204d5
Frankie Bagnardi <f.bagnardi@gmail.com>
2020-10-18 14:34:34 -0700

    docs: improve all examples and provide usage instructions

--- README.md
+++ README.md
@@ -11,19 +11,88 @@ They are implemented for all types.
 for types which have `Deref` and `DerefMut` implementations. They both borrow the receiver,
 and pass a reference of the `Deref::target` to the callback.
 
-## Examples
+## Usage
+
+You may either wildcard import it, or import specific traits. New items will not be added
+in semver compatible versions.
 
 ```rust
 use ov::*;
-let mut n = 5;
-assert_eq!(n.over(|n| n * 2), 10);
-n.over_mut(|n| {
-  *n *= 3
-});
-assert_eq!(n, 15);
+```
+or
+```rust
+use ov::Over;
+```
 
+## Examples
+
+A common use for this is with unary enum or struct constructors. These would otherwise
+be multiple statements with variables, or nested parenthesis.
+
+```rust
+use ov::*;
+use std::error::Error;
+use std::io::{Error as IoError, ErrorKind};
+
+fn maybe_string() -> Option<String> {
+    "foo".to_owned().over(Some)
+}
+assert_eq!(maybe_string(), Some("foo".to_owned()));
+
+// A helper is needed as `.over(Box::new)` always creates a Box<IoError>
+fn to_err_object(error: impl Error + Send + 'static) -> Box<dyn Error + Send> {
+    Box::new(error)
+}
+
+fn try_io_thing() -> Result<(), Box<dyn Error + Send>> {
+    IoError::new(ErrorKind::Other, "oh no!").over(to_err_object).over(Err)
+}
+```
+
+The `.over_mut` method can be used to perform some mutations on an arbitrary value,
+including a field of a struct.
+
+```rust
+use ov::*;
+struct S { field: i32 }
+let mut s = S { field: 5 };
+
+s.field.over_mut(|n| {
+    // n is &mut i32
+    *n *= 3;
+    *n += 1;
+});
+assert_eq!(s.field, 16);
+```
+
+The `over_deref` and `over_deref_mut` methods can be useful if you want to use a function
+that takes e.g. `&str` and you have a `String`.
+
+```rust
 let s = String::from("Hello, world!");
-// Note: this would fail if `s` is `String` or `&String`
-let len = s.over_deref(|s| str::len(s));
+// Note: this would fail if `my_str` were `String` or `&String`
+let len = s.over_deref(|my_str| str::len(my_str));
+// Can be shortened to this
+let len = s.over_deref(str::len);
 assert_eq!(len, 13);
 ```
+
+Another case for this is extracting the value from a mutable reference. For this, we
+can use [`std::mem::take`] or [`std::mem::replace`]. In the following example we have
+to use `replace` because `Command` doesn't impl `Default`.
+
+[`std::mem::take`]: https://doc.rust-lang.org/stable/std/mem/fn.take.html
+[`std::mem::replace`]: https://doc.rust-lang.org/stable/std/mem/fn.replace.html
+
+```rust
+use std::process::Command;
+fn assert_type<T>(_x: T) {}
+
+let command = Command::new("ls")
+    .arg("-a") // returns &mut Command
+    .arg("-l") // returns &mut Command
+    .over(|r| std::mem::replace(r, Command::new(""))); // returns Command
+
+assert_type::<Command>(command);
+
+License: MIT
--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,96 @@
 //! for types which have `Deref` and `DerefMut` implementations. They both borrow the receiver,
 //! and pass a reference of the `Deref::target` to the callback.
 //!
-//! # Examples
+//! # Usage
+//!
+//! You may either wildcard import it, or import specific traits. New items will not be added
+//! in semver compatible versions.
 //!
 //! ```
 //! use ov::*;
-//! let mut n = 5;
-//! assert_eq!(n.over(|n| n * 2), 10);
-//! n.over_mut(|n| {
-//!   *n *= 3
-//! });
-//! assert_eq!(n, 15);
+//! ```
+//! or
+//! ```
+//! use ov::Over;
+//! ```
 //!
+//! # Examples
+//!
+//! A common use for this is with unary enum or struct constructors. These would otherwise
+//! be multiple statements with variables, or nested parenthesis.
+//!
+//! ```
+//! use ov::*;
+//! use std::error::Error;
+//! use std::io::{Error as IoError, ErrorKind};
+//!
+//! fn maybe_string() -> Option<String> {
+//!     "foo".to_owned().over(Some)
+//! }
+//! assert_eq!(maybe_string(), Some("foo".to_owned()));
+//!
+//! // A helper is needed as `.over(Box::new)` always creates a Box<IoError>
+//! fn to_err_object(error: impl Error + Send + 'static) -> Box<dyn Error + Send> {
+//!     Box::new(error)
+//! }
+//!
+//! fn try_io_thing() -> Result<(), Box<dyn Error + Send>> {
+//!     IoError::new(ErrorKind::Other, "oh no!").over(to_err_object).over(Err)
+//! }
+//! ```
+//!
+//! The `.over_mut` method can be used to perform some mutations on an arbitrary value,
+//! including a field of a struct.
+//!
+//! ```
+//! use ov::*;
+//! struct S { field: i32 }
+//! let mut s = S { field: 5 };
+//!
+//! s.field.over_mut(|n| {
+//!     // n is &mut i32
+//!     *n *= 3;
+//!     *n += 1;
+//! });
+//! assert_eq!(s.field, 16);
+//! ```
+//!
+//! The `over_deref` and `over_deref_mut` methods can be useful if you want to use a function
+//! that takes e.g. `&str` and you have a `String`.
+//!
+//! ```
+//! # use ov::*;
 //! let s = String::from("Hello, world!");
-//! // Note: this would fail if `s` is `String` or `&String`
-//! let len = s.over_deref(|s| str::len(s));
+//! // Note: this would fail if `my_str` were `String` or `&String`
+//! let len = s.over_deref(|my_str| str::len(my_str));
+//! // Can be shortened to this
+//! let len = s.over_deref(str::len);
 //! assert_eq!(len, 13);
 //! ```
-
+//!
+//! Another case for this is extracting the value from a mutable reference. For this, we
+//! can use [`std::mem::take`] or [`std::mem::replace`]. In the following example we have
+//! to use `replace` because `Command` doesn't impl `Default`.
+//!
+//! [`std::mem::take`]: https://doc.rust-lang.org/stable/std/mem/fn.take.html
+//! [`std::mem::replace`]: https://doc.rust-lang.org/stable/std/mem/fn.replace.html
+//!
+//! ```
+//! # use ov::*;
+//! use std::process::Command;
+//! fn assert_type<T>(_x: T) {}
+//!
+//! let command = Command::new("ls")
+//!     .arg("-a") // returns &mut Command
+//!     .arg("-l") // returns &mut Command
+//!     .over(|r| std::mem::replace(r, Command::new(""))); // returns Command
+//!
+//! assert_type::<Command>(command);
+/// ```
 use std::ops::Deref;
 use std::ops::DerefMut;
 
+/// Transform by value (`self`)
 pub trait Over: Sized {
     fn over<F, Ret>(self, f: F) -> Ret
     where
@@ -40,6 +110,7 @@ pub trait Over: Sized {
 
 impl<T> Over for T {}
 
+/// Transform by shared reference (`&self`)
 pub trait OverRef {
     fn over_ref<F, Ret>(&self, f: F) -> Ret
     where
@@ -51,6 +122,7 @@ pub trait OverRef {
 
 impl<T> OverRef for T {}
 
+/// Transform by unique reference (`&mut self`)
 pub trait OverMut {
     fn over_mut<F, Ret>(&mut self, f: F) -> Ret
     where
@@ -62,6 +134,7 @@ pub trait OverMut {
 
 impl<T> OverMut for T {}
 
+/// Transform by deref coercion (`&self |> Deref::deref(self)`)
 pub trait OverDeref: Deref {
     fn over_deref<F, Ret>(&self, f: F) -> Ret
     where
@@ -80,6 +153,7 @@ where
     }
 }
 
+/// Transform by mutable deref coercion (`&self |> DerefMut::deref_mut(self)`)
 pub trait OverDerefMut: DerefMut {
     fn over_deref_mut<F, Ret>(&mut self, f: F) -> Ret
     where
